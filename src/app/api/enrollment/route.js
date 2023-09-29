0aae3f2bb36083e834c0e11080bbf6665438eccc
Bank5807 <TAnWiPaSa@gmail.com>
2023-09-29 13:42:17 +0700

    add all requirements.

--- src/app/api/enrollment/route.js
+++ src/app/api/enrollment/route.js
@@ -83,34 +83,52 @@ export const POST = async (request) => {
   const prisma = getPrisma();
   //1.check if courseNo does not exist on database
   //send this response back if courseNo does not exist
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Course number does not exist",
-  //   },
-  //   { status: 400 }
-  // );
-
-  //2.check if such student enroll that course already (both "studentId" and "courseNo" exists on enrollment collection)
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "You already registered this course",
-  //   },
-  //   { status: 400 }
-  // );
-
-  //3.if conditions above are not met, perform inserting data here
-  // await prisma.enrollment.create({
-  //   data:{
-  //     ...
-  //   }
-  // })
-
-  return NextResponse.json({
-    ok: true,
-    message: "You has enrolled a course successfully",
+  const course = await prisma.courses.findUnique({
+    where: {
+      courseNo: courseNo,
+    },
   });
+
+  if (!course) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Course number does not exist",
+      },
+      { status: 400 }
+    );
+  }
+  //2.check if such student enroll that course already (both "studentId" and "courseNo" exists on enrollment collection)
+  const existingEnrollment = await prisma.enrollment.findUnique({
+    where: {
+      studentId: studentId,
+      courseNo: courseNo,
+    },
+  });
+  if (existingEnrollment) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "You already registered this course",
+      },
+      { status: 400 }
+    );
+  }
+  //3.if conditions above are not met, perform inserting data here
+  await prisma.enrollment.create({
+    data: {
+      studentId: studentId,
+      courseNo: courseNo,
+    },
+  });
+
+  return NextResponse.json(
+    {
+      ok: true,
+      message: "You has enrolled a course successfully",
+    },
+    { status: 200 }
+  );
 };
 
 export const DELETE = async (request) => {
